67a2334a28d9edfe9bca30e21456275477b02264
Guilherme Vogler <109769831+VoglerGui@users.noreply.github.com>
2024-03-02 19:46:08 -0300

    Create ex4.java

---
+++ ex4.java
@@ -0,0 +1,39 @@
+package problems;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		Employee emp = new Employee();
+		
+		
+		System.out.print("Name: ");
+		emp.name = sc.nextLine();
+		System.out.print("Gross salary: ");
+		emp.grossSalary = sc.nextDouble();
+		System.out.print("Tax: ");
+		emp.tax = sc.nextDouble();
+		
+		System.out.println();
+		System.out.println("Employee: " + emp);
+				
+		System.out.println();
+		System.out.print("Which percentage to increase salary? ");
+		double percentage = sc.nextDouble();
+		emp.IncreaseSalary(percentage);
+		
+		System.out.println();
+		System.out.println("Update data: " + emp);
+		
+		
+		sc.close();
+	}
+}
